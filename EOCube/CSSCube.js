d598984fc76997e1c27659f2f382596e5b2a4ec2
teoidus <Guest@mhs-macbook-air.gateway.pace.com>
2016-05-29 02:51:47 -0500

    Chg oh basis

--- EOCube/CSSCube.js
+++ EOCube/CSSCube.js
@@ -17,9 +17,9 @@ function CSSCube(id, colorScheme, size, div) { // colorScheme = rgb, [U L F R B 
     var face = Math.floor(i/9);
     this.stickers.push(this.colors[face]);
 
-    var rotations = [[90,0,0],[0,90,0],[0,0,0],[0,-90,0],[180,0,0],[-90,0,0]];
-    var rotation = rotations[face];
-    this.rotations.push(new Rotation(rotation[0], rotation[1], rotation[2]));
+    var rotations = [{angle:90, axis:0}, {angle:90, axis:2}, {angle:0, axis:0},
+                     {angle:-90, axis:2}, {angle:180, axis:0}, {angle:-90, axis:0}];
+    this.rotations.push(new Rotation(rotations[face]));
 
     var sticker = i % 9; // location of sticker relative to face
     var origins = [ // coords = cube center - sticker center
@@ -47,16 +47,17 @@ function CSSCube(id, colorScheme, size, div) { // colorScheme = rgb, [U L F R B 
     figure.style.height = (this.size/3) + "px";
     //figure.style.border = "1px solid black";
     figure.style.backgroundColor = this.stickers[i];
-    figure.style.opacity = 1;
+    figure.style.opacity = 0.8;
     //figure.style.display = "block";
     this.div.appendChild(figure);
-    figure.style.transition = "transform 0.5s";
+    figure.style.transition = "transform 0.3s";
     figure.style.transformOrigin = this.origins[i].toCSS();
     figure.style.transform = this.rotations[i].toCSS();
 
-    console.log("Face",face,"rot",this.rotations[i].toCSS(),
+    /*console.log("Face",face,"rot",this.rotations[i].toCSS(),
             "origin",this.origins[i].toCSS(),
-            "initial",JSON.stringify(this.initials[i]));
+            "initial",JSON.stringify(this.initials[i]),
+            figure.style.backgroundColor);*/
   }
 }
 
@@ -68,21 +69,21 @@ CSSCube.prototype.updateTransforms = function() {
 
 CSSCube.prototype.testX = function() {
   for (var i = 0; i < this.rotations.length; i++) {
-    this.rotations[i].x += 90;
+    this.rotations[i].addRotation({angle: 90, axis: 0});
   }
   this.updateTransforms();
 }
 
 CSSCube.prototype.testY = function() {
   for (var i = 0; i < this.rotations.length; i++) {
-    this.rotations[i].y -= 90;
+    this.rotations[i].addRotation({angle: 90, axis: 1});
   }
   this.updateTransforms();
 }
 
 CSSCube.prototype.testZ = function() {
   for (var i = 0; i < this.rotations.length; i++) {
-    this.rotations[i].z += 90;
+    this.rotations[i].addRotation({angle: 90, axis: 2});
   }
   this.updateTransforms();
 }
@@ -109,17 +110,109 @@ RelativeOrigin.prototype.toCSS = function() {
   return (this.px*100) + "% " + (this.py*100) + "% " + (this.pz*this.dx) + "px";
 }
 
-// Represents rotations in terms of angles rx,ry,rz on x,y,z cartesian axes
-function Rotation(rx, ry, rz) {
-  this.x = rx; this.y = ry; this.z = rz;
+// Represent rotations as sequence of transformations
+function Rotation(t) {
+  this.transforms = [t]; // t = { angle, axis: 0, 1, or 2 }
+}
+
+Rotation.prototype.addRotation = function(t) {
+  if (this.transforms.length > 0
+    && this.transforms[this.transforms.length-1].axis == t.axis)
+    this.transforms[this.transforms.length-1].angle += t.angle;
+  else
+    this.transforms.push(t);
+  for (var i = 0; i < this.transforms.length; i++) {
+    if (this.transforms[i].angle > 0)
+      this.transforms[i].angle = this.transforms[i].angle % 360;
+  }
 }
 
 // Chg basis to account for fact that rotate_() is not commutative
 Rotation.prototype.toCSS = function() {
-  return "rotateX(" + this.x + "deg) rotateY(" + this.y + "deg) rotateZ(" + this.z + "deg)";
+  var unitVectors = [[1,0,0], [0,1,0], [0,0,1]]; // cartesian system
+  var basis = [[1,0,0], [0,0,-1], [0,-1,0]]; // graphics basis vectors
+
+    var removeZero = function(x) { return (Math.abs(x) < 0.001) ? 0 : x };
+    var removeZeros = function(x) {
+      var r = [];
+      for (var i = 0; i < x.length; i++) {
+        r.push(removeZero(x[i]));
+      }
+      return r;
+    };
+
+  //console.log("Start toCSS");
+  var axes = []; // axes of rotation
+  for (var i = 0; i < this.transforms.length; i++) {
+    var theta = this.transforms[i].angle * Math.PI/180;
+    var axis = Matrix.chgBasis(basis, unitVectors[this.transforms[i].axis]);
+    axes.push(axis);
+    switch (this.transforms[i].axis) {
+      case 0: var t = [
+          [1, 0, 0],
+          [0, Math.cos(theta), Math.sin(theta)],
+          [0, Math.cos(theta + Math.PI/2), Math.sin(theta + Math.PI/2)]
+        ]; break;
+      case 1: var t = [
+          [Math.sin(theta + Math.PI/2), 0, Math.cos(theta + Math.PI/2)],
+          [0, 1, 0],
+          [Math.sin(theta), 0, Math.cos(theta)]
+        ]; break;
+      case 2: var t = [
+          [Math.cos(theta), Math.sin(theta), 0],
+          [Math.cos(theta + Math.PI/2), Math.sin(theta + Math.PI/2), 0],
+          [0, 0, 1],
+        ]; break;
+    }
+    basis = Matrix.transformBasis(t, basis);
+    //console.log("Pushed axis: " + JSON.stringify(removeZeros(axis)));
+    //console.log("New basis: " + JSON.stringify([
+    //  removeZeros(basis[0]),
+    //  removeZeros(basis[1]),
+    //  removeZeros(basis[2])]));
+  }
+
+  var s = "";
+  for (var i = 0; i < axes.length; i++) {
+    axes[i] = removeZeros(axes[i]);
+    s += "rotate3d(" + axes[i].join(",") + "," + this.transforms[i].angle + "deg) ";
+  }
+  s = s.trim();
+  //console.log(s);
+  return s;
 }
 
 Matrix = {
   dot: function(a, b) { var c = 0; for (var i = 0; i < a.length; i++) c += a[i]*b[i]; return c },
-  chgBasis: function(b, a) { return [Matrix.dot(b[0],a), Matrix.dot(b[1],a), Matrix.dot(b[2],a)] }
+  scale: function(a, b) { var c = []; for (var i = 0; i < b.length; i++) c.push(a*b[i]); return c },
+  chgBasis: function(b, a) { return [Matrix.dot(b[0],a), Matrix.dot(b[1],a), Matrix.dot(b[2],a)] },
+  transformBasis: function(t, b) {
+    var result = Matrix.create(t.length, t[0].length);
+    for (var i = 0; i < t.length; i++) {
+      for (var j = 0; j < t[0].length; j++) {
+        result[i][j] = Matrix.dot(t[j], b[i]);
+      }
+    }
+    return result;
+  },
+  create(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = Matrix.create.apply(this, args);
+    }
+
+    return arr;
+  },
+  transpose: function(a) {
+    var result = Matrix.create(a.length, a[0].length);
+    for (var i = 0; i < a.length; i++) {
+      for (var j = 0; j < a[0].length; j++) {
+        result[j][i] = a[i][j];
+      }
+    }
+    return result;
+  }
 }
