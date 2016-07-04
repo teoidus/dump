ccfd42e949eacb3f60125c2e79a271e864bbebac
teoidus <jl5647382910@gmail.com>
2016-07-04 11:37:12 -0500

    Chronos timeline

--- Chronos/Chronos.js
+++ Chronos/Chronos.js
@@ -1,3 +1,17 @@
+function msToTime(duration) {
+    var milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+// from https://coderwall.com/p/wkdefg/converting-milliseconds-to-hh-mm-ss-mmm
+
 function Chronos(agenda, dt, canvas) {
   this.parseDuration = function(hm) {
     var tokens = hm.split(":"); // h:m
@@ -60,6 +74,7 @@ Chronos.prototype.render = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   
   var timelineY = this.canvas.height * 7/8;
+  var tickY = timelineY - this.canvas.height/16;
   
   // Current event description
   var s = this.current.desc;
@@ -91,7 +106,12 @@ Chronos.prototype.render = function() {
   var x = this.t/this.totalTime * this.canvas.width;
   this.ctx.strokeStyle = this.events[this.current.index].color;
   this.ctx.beginPath();
-  this.ctx.moveTo(x, timelineY);
-  this.ctx.lineTo(x, this.canvas.height);
+  this.ctx.moveTo(x, tickY);
+  this.ctx.lineTo(x, timelineY);
   this.ctx.stroke();
+  // Timestamp
+  this.ctx.textAlign = (this.t/this.totalTime > 0.5) ? "right" : "left"; 
+  var sign = (this.t/this.totalTime > 0.5) ? -1 : 1; 
+  this.ctx.font = this.canvas.height/16 + "px Arial";
+  this.ctx.fillText(msToTime(this.t), x + sign*this.canvas.width/256, timelineY - this.canvas.height/128);
 }
